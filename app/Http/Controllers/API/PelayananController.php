1894b582b4d70b6f8f263838e461eb08af6dc9c2
faruqrahmadani <faruq.rahmadani@tokopedia.com>
2019-10-25 21:05:20 +0700

    find data pelayanan

--- app/Http/Controllers/API/PelayananController.php
+++ app/Http/Controllers/API/PelayananController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Pelayanan;
+use HCrypt;
 use Redis;
 
 class PelayananController extends APIController
@@ -13,11 +14,29 @@ class PelayananController extends APIController
         $pelayanan = Redis::get("pelayanan:all");
         if (!$pelayanan) {
             $pelayanan = Pelayanan::all();
+            if (!$pelayanan) {
+                return $this->returnController("error", "failed get pelayanan data");
+            }
             Redis::set("pelayanan:all", $pelayanan);
         }
-        if (!$pelayanan) {
-            return $this->returnController("error", "failed get pelayanan data");
+        return $this->returnController("ok", $pelayanan);
+    }
+
+    public function find($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
         }
+
+        $pelayanan = Redis::get("pelayanan:$id");
+        if (!$pelayanan) {
+            $pelayanan = Pelayanan::find($id);
+            if (!$pelayanan){
+                return $this->returnController("error", "failed find data pelayanan");
+            }
+            Redis::set("pelayanan:$id", $pelayanan);
+        }
+
         return $this->returnController("ok", $pelayanan);
     }
 
