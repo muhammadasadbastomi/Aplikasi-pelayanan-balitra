2824b42071e5da89127bd3b3c0bd1533069e7c67
tri angga utama <trianggategarutama@gmail.com>
2019-10-31 16:05:25 +0800

    get API pelayanan

---
+++ database/migrations/2019_10_24_060416_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->tinyInteger('role')->default(1);
+            $table->tinyInteger('status')->default(0);
+            $table->string('foto')->length('255')->default('default.jpg');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
