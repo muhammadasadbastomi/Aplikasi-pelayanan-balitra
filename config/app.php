2d99465b35498e76c8fde7eabb93b6d0c4f18518
Toms <toms@Tomss-MacBook-Pro.local>
2019-11-01 07:32:31 +0800

    fix capitalize typo

--- config/app.php
+++ config/app.php
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'HCrypt' => app\Helpers\CryptHelper::class,
+        'HCrypt' => App\Helpers\CryptHelper::class,
 
     ],
 
