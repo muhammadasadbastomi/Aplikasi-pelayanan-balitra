6bd9df17a02e90f91290c573d8c5ed9568a68ffa
Toms <toms@Tomss-MacBook-Pro.local>
2019-11-11 23:55:30 +0800

    add uuid for create pelayanan data

--- app/Pelayanan.php
+++ app/Pelayanan.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use HCrypt;
+
 class Pelayanan extends Model
 {
     protected $fillable = ['name', 'price'];
+
+    protected $hidden = [
+        'id'
+    ];
+
+    protected $appends = array('uuid');
+
+    public function getUuidAttribute()
+    {
+        // return Hash::make($this->id);
+        return HCrypt::encrypt($this->id);
+    }
 }
