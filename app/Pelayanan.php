a69dd84baf563946ff25d38ca16e26079bf234a0
Toms <toms@pop-os.localdomain>
2019-11-15 23:40:46 +0800

    delete useless comment and beutify code

--- app/Pelayanan.php
+++ app/Pelayanan.php
@@ -9,16 +9,11 @@ use HCrypt;
 class Pelayanan extends Model
 {
     protected $fillable = ['name', 'price'];
-
-    protected $hidden = [
-        'id'
-    ];
-
+    protected $hidden = ['id'];
     protected $appends = array('uuid');
 
     public function getUuidAttribute()
     {
-        // return Hash::make($this->id);
         return HCrypt::encrypt($this->id);
     }
 }
