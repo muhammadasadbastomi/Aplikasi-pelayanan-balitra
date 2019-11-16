b4bde286576722e2223a8bd2623830fc84e6e9fa
muhammadasadbastomi <tomycules@gmail.com>
2019-11-16 08:17:29 +0800

    create method update pelanggan data

--- app/Http/Controllers/API/PelangganController.php
+++ app/Http/Controllers/API/PelangganController.php
@@ -45,4 +45,32 @@ class PelangganController extends Controller
         Redis::del("pelanggan:all");
         return $this->returnController("ok", $merge);
     }
+
+    public function update($uuid, Request $req){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+
+        $pelanggan = pelanggan::find($id);
+        $user_id = $pelanggan->user_id;
+        $user = user::findOrFail($user_id);
+        if (!$user) {
+            return $this->returnController("error", "failed find data pelanggan");
+        }
+
+        $u_user = $user->update($req->all());
+        $u_pelanggan = $pelanggan->update($req->all());
+        if (!$u_user && $u_pelanggan) {
+            return $this->returnController("error", "failed find data pelanggan");
+        }
+        $merge = (['user' => $u_user, 'pelanggan' => $u_pelanggan]);
+
+        Redis::del("user:all");
+        Redis::set("user:$user_id", $u_user);
+        Redis::del("pelanggan:all");
+        Redis::set("pelanggan:$id", $u_pelanggan);
+
+        return $this->returnController("ok", $merge);
+    }
 }
