b1f9e121a52e5cf841a370a0be03652a6cc16776
muhammadasadbastomi <tomycules@gmail.com>
2019-11-16 08:06:57 +0800

    create method get pelanggans data

--- app/Http/Controllers/API/PelangganController.php
+++ app/Http/Controllers/API/PelangganController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PelangganController extends Controller
 {
-    //
+    public function get(){
+        $pelanggan = Redis::get("pelanggan:all");
+        if (!$pelanggan) {
+            $pelanggan = pelanggan::all();
+            if (!$pelanggan) {
+                return $this->returnController("error", "failed get pelanggan data");
+            }
+            Redis::set("pelanggan:all", $pelanggan);
+        }
+        return $this->returnController("ok", $pelanggan);
+    }
 }
