0ca31993a26a8252a2935cad32f3405b64cf2e48
muhammadasadbastomi <tomycules@gmail.com>
2019-11-22 05:43:45 +0800

    fix error unexpected end of file

--- app/Pelanggan.php
+++ app/Pelanggan.php
@@ -9,18 +9,13 @@ use HCrypt;
 class Pelanggan extends Model
 {
     protected $fillable = [
-        'kd_pelanggan', 'alamat',  'telepon'
+        'uuid','kd_pelanggan', 'alamat',  'telepon'
     ];
     protected $hidden = [
         'id', 'user_id'
     ];
-    protected $appends = array('uuid');
-
-    public function getUuidAttribute()
-    {
-        return HCrypt::encrypt($this->id);
-    }
 
     public function user(){
       return $this->HasOne('App\User');
+    }
 }
