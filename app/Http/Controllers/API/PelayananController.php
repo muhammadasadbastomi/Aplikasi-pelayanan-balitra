90967dd5d2686e72ddd29f31cc84b0f0b5f9d687
muhammadasadbastomi <tomycules@gmail.com>
2020-01-01 23:01:38 +0800

    add value jenis pelayanan_id

--- app/Http/Controllers/API/PelayananController.php
+++ app/Http/Controllers/API/PelayananController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers\API;
+
 use Illuminate\Support\Facades\Redis;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -10,9 +12,8 @@ class PelayananController extends APIController
 {
     public function get(){
         $pelayanan = json_decode(redis::get("pelayanan::all"));
-        // $pelayanan = Redis::get("pelayanan:all");
         if (!$pelayanan) {
-            $pelayanan = Pelayanan::all();
+            $pelayanan = pelayanan::with('jenispelayanan')->get();
             if (!$pelayanan) {
                 return $this->returnController("error", "failed get pelayanan data");
             }
@@ -22,90 +23,81 @@ class PelayananController extends APIController
     }
 
     public function find($uuid){
-        $id = Hcrypt::decrypt($uuid);
+        $id = HCrypt::decrypt($uuid);
         if (!$id) {
             return $this->returnController("error", "failed decrypt uuid");
         }
-
         $pelayanan = Redis::get("pelayanan:$id");
         if (!$pelayanan) {
-            $pelayanan = Pelayanan::find($id);
+            $pelayanan = pelayanan::with('jenispelayanan')->where('id',$id)->first();
             if (!$pelayanan){
                 return $this->returnController("error", "failed find data pelayanan");
             }
             Redis::set("pelayanan:$id", $pelayanan);
         }
-
         return $this->returnController("ok", $pelayanan);
     }
 
     public function create(Request $req){
-        $create = new Pelayanan;
-        
-        $create->name     = $req->name;
-        $create->price    = $req->price;
-        $create->save();
+        // $seksi = Seksi::create($req->all());
+        $pelayanan = new pelayanan;
+        // decrypt foreign key id
+        $pelayanan->jenispelayanan_id = Hcrypt::decrypt($req->jenispelayanan_id);
+        $pelayanan->name = $req->name;
+        $pelayanan->price = $req->price;
 
-        $id= $create->id;
-        $uuid = HCrypt::encrypt($id);
-        $setuuid = Pelayanan::findOrFail($id);
+        $pelayanan->save();
+
+        //set uuid
+        $pelayanan_id = $pelayanan->id;
+        $uuid = HCrypt::encrypt($pelayanan_id);
+        $setuuid = pelayanan::findOrFail($pelayanan_id);
         $setuuid->uuid = $uuid;
         $setuuid->update();
-       
-        if (!$create) {
+        if (!$pelayanan) {
             return $this->returnController("error", "failed create data pelayanan");
         }
-        
         Redis::del("pelayanan:all");
-        return $this->returnController("ok", $create);
+        Redis::set("pelayanan:all", $pelayanan);
+        return $this->returnController("ok", $pelayanan);
     }
 
     public function update($uuid, Request $req){
-        $id = Hcrypt::decrypt($uuid);
+        $id = HCrypt::decrypt($uuid);
         if (!$id) {
             return $this->returnController("error", "failed decrypt uuid");
         }
-
-        $update = Pelayanan::findOrFail($id);
-        if (!$update){
-                return $this->returnController("error", "failed find data pelayanan");
-            }
-        $update->name = $req->name;
-        $update->price = $req->price;
-        $update->update();
-
-        if (!$update) {
-            return $this->returnController("error", "failed find data pelayanan");
-        }
-        
-        Redis::del("pelayanan:all");
-        Redis::set("pelayanan:$id", $update);
-
-        return $this->returnController("ok", $update);
-    }
-
-    public function delete($uuid){
-        $id = Hcrypt::decrypt($uuid);
-        if (!$id) {
-            return $this->returnController("error", "failed decrypt uuid");
-        }
-
-        $pelayanan = Pelayanan::find($id);
+        $pelayanan = pelayanan::findOrFail($id);
+        $pelayanan->jenispelayanan_id = Hcrypt::decrypt($req->jenispelayanan_id);
+        $pelayanan->name = $req->name;
+        $pelayanan->price = $req->price;
+        $pelayanan->update();
         if (!$pelayanan) {
             return $this->returnController("error", "failed find data pelayanan");
         }
+        $pelayanan = pelayanan::with('jenispelayanan')->where('id',$id)->first();
+        Redis::del("pelayanan:all");
+        Redis::set("pelayanan:$id", $pelayanan);
+        return $this->returnController("ok", $pelayanan);
+    }
 
+    public function delete($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $pelayanan = pelayanan::find($id);
+        if (!$pelayanan) {
+            return $this->returnController("error", "failed find data pelayanan");
+        }
         // Need to check realational
         // If there relation to other data, return error with message, this data has relation to other table(s)
-
         $delete = $pelayanan->delete();
         if (!$delete) {
             return $this->returnController("error", "failed delete data pelayanan");
         }
-
         Redis::del("pelayanan:all");
         Redis::del("pelayanan:$id");
-
         return $this->returnController("ok", "success delete data pelayanan");
     }
 }
