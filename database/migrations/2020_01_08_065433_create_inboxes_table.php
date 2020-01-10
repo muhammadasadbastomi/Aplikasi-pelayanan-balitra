620c1a20450cb857a8e450d5d830c27e3bb155d9
muhammadasadbastomi <tomycules@gmail.com>
2020-01-10 15:55:59 +0800

    create migration pengujian

---
+++ database/migrations/2020_01_08_065433_create_inboxes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInboxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inboxes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedbigInteger('user_id');
+            $table->unsignedbigInteger('permohonan_id')->nullable();
+            $table->text('uuid')->nullable();
+            $table->string('subjek')->length(50);
+            $table->text('keterangan');
+            $table->tinyInteger('status')->default(0);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('permohonan_id')->references('id')->on('permohonans')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inboxes');
+    }
+}
