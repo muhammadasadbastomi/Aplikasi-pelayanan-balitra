6bf8d5a4af4aff0dbfb0bc556e8f5e60495633aa
muhammadasadbastomi <tomycules@gmail.com>
2020-01-12 19:26:14 +0800

    add library hcrypt

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Inbox;
 use App\Permohonan;
+use HCrypt;
 use Illuminate\Http\Request;
 
 class customerController extends Controller
