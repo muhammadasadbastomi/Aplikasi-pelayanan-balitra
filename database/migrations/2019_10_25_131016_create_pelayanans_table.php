14943dbe48482879507d83f79b6f35dbb2001b7e
muhammadasadbastomi <tomycules@gmail.com>
2020-01-23 14:38:35 +0800

    add field satuan

--- database/migrations/2019_10_25_131016_create_pelayanans_table.php
+++ database/migrations/2019_10_25_131016_create_pelayanans_table.php
@@ -18,6 +18,7 @@ class CreatePelayanansTable extends Migration
             $table->text('uuid')->length(100)->nullable();
             $table->unsignedBigInteger('jenis_pelayanan_id');
             $table->string('name')->length(100);
+            $table->string('satuan');
             $table->double('price');
             $table->foreign('jenis_pelayanan_id')->references('id')->on('jenis_pelayanans')->onDelete('cascade');
             $table->timestamps();
