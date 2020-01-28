4b5481dd77eadd8671cfea43ffc986ab1f4dfa51
muhammadasadbastomi <tomycules@gmail.com>
2020-01-28 20:44:55 +0700

    fix

--- app/Pelayanan.php
+++ app/Pelayanan.php
@@ -9,10 +9,14 @@ use HCrypt;
 class Pelayanan extends Model
 {
     protected $fillable = ['uuid','name', 'price'];
-    protected $hidden = ['id','jenis_pelayanan_id'];
+    protected $hidden = ['id','jenis_pelayanan_id','buah_id'];
 
     public function jenis_pelayanan(){
         return $this->BelongsTo('App\JenisPelayanan');
+      }    
+
+    public function buah(){
+        return $this->BelongsTo('App\Buah');
       }    
 
     public function detail_permohonan()
